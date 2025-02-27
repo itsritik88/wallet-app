b712532e1d3509c99c59fda2c56a596098795f67
ritik <itsritik88@gmail.com>
2025-02-27 21:04:49 +0530

    changed vit config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,5 +5,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-})
+  build: {
+    rollupOptions: {
+      input: 'index.html'
+    }
+  }
+});
 
